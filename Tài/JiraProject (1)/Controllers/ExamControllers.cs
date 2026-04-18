34b54dd9358e98df90d6e9095cd0708b5b8cf3c6
ttutai188 <truongtutai188@gmail.com>
2026-04-19 00:31:22 +0700

    Sprint 1:Trang chu + DS ky thi

---
+++ Tài/JiraProject (1)/Controllers/ExamControllers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YourProjectName.Controllers
+{
+    [ApiController]
+    [Route("api/Exams")] // [AC 3]: Route chuẩn theo yêu cầu
+    public class ExamControllers : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // [AC 1]: Bổ sung trường "time" cho mỗi đối tượng
+            var exams = new[]
+            {
+                new { subject = "Toán Học", date = "2024-06-15", time = "07:30", room = "Phòng A1" },
+                new { subject = "Tiếng Anh", date = "2024-06-16", time = "13:30", room = "Phòng B2" },
+                new { subject = "Ngữ Văn", date = "2024-06-17", time = "08:00", room = "Phòng C3" },
+                new { subject = "Vật Lý", date = "2024-06-18", time = "14:00", room = "Phòng D4" },
+                new { subject = "Hóa Học", date = "2024-06-19", time = "09:15", room = "Phòng E5" }
+            };
+
+            // Trả về mảng dữ liệu
+            return Ok(exams);
+
+            // Để test thông báo trống, bạn có thể tạm comment dòng trên và dùng:
+            // return Ok(new object[] { }); 
+        }
+    }
+}
---
+++ Tài/JiraProject (1)/Program.cs
@@ -0,0 +1,30 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// 1. Cấu hình Services
+builder.Services.AddCors(options => options.AddDefaultPolicy(p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// 2. Cấu hình HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// FIX: Thêm UseDefaultFiles TRƯỚC UseStaticFiles
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseHttpsRedirection();
+
+// Thứ tự chuẩn cho Middleware: Cors -> Authentication -> Authorization
+app.UseCors();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
